3418e7fb29d513d2bfba90bf920125fb45970bd5
Derek Roberts <derek.roberts@gmail.com>
2025-06-02 12:38:50 -0700

    chore: obscure internal resources (#136)

--- metabase/run_app.sh
+++ metabase/run_app.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-DB_HOST_PORT_ENV=${DB_HOST_PORT_ENV}
 cert_folder="/opt"
 MAX_HEAP=${MAX_HEAP:-750m}
 MIN_HEAP=${MIN_HEAP:-750m}
-echo "DB_HOST_PORT_ENV is $DB_HOST_PORT_ENV"
+
+# Verify that the required environment variables are set
 if [ -z "$DB_HOST_PORT_ENV" ]; then
-  DB_HOST_PORT_ENV=nrcdb01.bcgov:1543,nrcdb03.bcgov:1543,nrkdb01.bcgov:1543,nrkdb03.bcgov:1543,nrcdb02.bcgov:1543,nrkdb02.bcgov:1543
+  echo "DB_HOST_PORT_ENV is not set.  Exiting."
+  exit 1
 fi
-  IFS=','
-  read -ra DB_HOST_PORT_ARRAY <<< "${DB_HOST_PORT_ENV}"
-  for DB_HOST_PORT in "${DB_HOST_PORT_ARRAY[@]}"; do
-    IFS=':'
-    read -ra strarr <<<"${DB_HOST_PORT}"
-    DB_HOST="${strarr[0]}"
-    DB_PORT="${strarr[1]}"
-    openssl s_client -connect "${DB_HOST}:${DB_PORT}" -showcerts </dev/null | openssl x509 -outform pem >"$cert_folder/${DB_HOST}.pem" || exit 1
-    openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/${DB_HOST}.der" || exit 1
-    keytool -import -alias "orakey-${DB_HOST}-1" -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file "$cert_folder/${DB_HOST}.der" -noprompt || exit 1
-  done
+
+IFS=','
+read -ra DB_HOST_PORT_ARRAY <<< "${DB_HOST_PORT_ENV}"
+for DB_HOST_PORT in "${DB_HOST_PORT_ARRAY[@]}"; do
+  IFS=':'
+  read -ra strarr <<<"${DB_HOST_PORT}"
+  DB_HOST="${strarr[0]}"
+  DB_PORT="${strarr[1]}"
+  openssl s_client -connect "${DB_HOST}:${DB_PORT}" -showcerts </dev/null | openssl x509 -outform pem >"$cert_folder/${DB_HOST}.pem" || exit 1
+  openssl x509 -outform der -in "$cert_folder/${DB_HOST}.pem" -out "$cert_folder/${DB_HOST}.der" || exit 1
+  keytool -import -alias "orakey-${DB_HOST}-1" -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file "$cert_folder/${DB_HOST}.der" -noprompt || exit 1
+done
 
 echo -e "\033[1;33m=====================================\033[0m" # Bright Yellow
 echo -e "\033[0;36m  _   _ ____      __  __ ____  \033[0m" # Cyan
