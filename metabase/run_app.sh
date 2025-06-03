22022ad0d378e2f64044d90dc1766a8c91a030c8
Derek Roberts <derek.roberts@gmail.com>
2025-06-03 16:24:35 -0700

    chore: script cleanup (#137)
    * chore(ci): drop extra script from config map
    
    * Allow script to run withot DB_HOST_PORT_ENV
    
    * Remove startup configmap reference
    
    * Startup script
    
    * Startup script, use in-place
    
    * Startup script, use in-place
    
    * fix: add conditionals for optional value
    
    * fix char
    
    * chore: remove fancy
    
    * fix: path of configmap for logging
    
    * add missing quota
    
    * debug
    
    * debug
    
    * removed echo
    
    * fix: quotes in env var
    
    * use env
    
    * fix: quotes position
    
    * debug
    
    * quotes
    
    * steal from Derek
    
    * still trying
    
    * chore: try literal vs string to escape the escape
    
    * remove echo, go back to secrets
    
    ---------
    
    Co-authored-by: Om Mishra <32200996+mishraomp@users.noreply.github.com>

--- metabase/run_app.sh
+++ metabase/run_app.sh
@@ -5,8 +5,9 @@ MIN_HEAP=${MIN_HEAP:-750m}
 
 # Verify that the required environment variables are set
 if [ -z "$DB_HOST_PORT_ENV" ]; then
-  echo "DB_HOST_PORT_ENV is not set.  Exiting."
-  exit 1
+  echo -e "\n---"
+  echo -e "Warning: DB_HOST_PORT_ENV is not set. \n"
+
 fi
 
 IFS=','
@@ -21,18 +22,10 @@ for DB_HOST_PORT in "${DB_HOST_PORT_ARRAY[@]}"; do
   keytool -import -alias "orakey-${DB_HOST}-1" -keystore "${JAVA_HOME}"/lib/security/cacerts -storepass changeit -file "$cert_folder/${DB_HOST}.der" -noprompt || exit 1
 done
 
-echo -e "\033[1;33m=====================================\033[0m" # Bright Yellow
-echo -e "\033[0;36m  _   _ ____      __  __ ____  \033[0m" # Cyan
-echo -e "\033[0;36m | \ | |  _ \     |  \/  | __ ) \033[0m" # Cyan
-echo -e "\033[0;36m |  \| | |_) |    | |\/| |  _ \ \033[0m" # Cyan
-echo -e "\033[0;36m | |\  |  _ <     | |  | | |_) |\033[0m" # Cyan
-echo -e "\033[0;36m |_| \_|_| \_\    |_|  |_|____/ \033[0m" # Cyan
-echo -e "\033[0;36m                        ${NR_MB_VERSION} \033[0m" # Cyan
-echo -e "\033[1;33m=====================================\033[0m" # Bright Yellow
+echo "NR Metabase started at: $(date +'%Y-%m-%d %H:%M:%S') with version: ${NR_MB_VERSION}"
 
-if [ -f /mnt/conf/log4j2.xml ]; then
+if [ -f /config/log4j2.xml ]; then
   java -Duser.name=metabase "-Xms${MIN_HEAP}" "-Xmx${MAX_HEAP}" -XX:TieredStopAtLevel=2 -XX:+UseZGC  -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MaxMetaspaceSize=350m -XX:ParallelGCThreads=2 -Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -XX:CICompilerCount=2 -XX:+ExitOnOutOfMemoryError -Dlog4j.configurationFile=file:/config/log4j2.xml -jar metabase.jar
 else
   java -Duser.name=metabase "-Xms${MIN_HEAP}" "-Xmx${MAX_HEAP}" -XX:TieredStopAtLevel=2 -XX:+UseZGC  -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -XX:MaxMetaspaceSize=350m -XX:ParallelGCThreads=2 -Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -XX:CICompilerCount=2 -XX:+ExitOnOutOfMemoryError -jar metabase.jar
 fi
-
